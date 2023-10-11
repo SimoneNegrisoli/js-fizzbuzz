95dd16d2b59377e06af410f539ef0aa1decd7c5e
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-11 17:22:54 +0200

    costant and for

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Consegna:
+// > Scrivi un programma che stampi in console i numeri da 1 a 100,
+// > ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// > Come faccio a sapere se un numero è divisibile per un altro?
+// > Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+//  1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+//  2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare.
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+// Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+
+
+
+
+// Costanti
+
+const fizz = 'Fizz';
+const buzz = 'Buzz';
+
+// dove lo voglio mettere
+const boxEl = document.getElementById('boxcontainer');
+let valore = "";
+let boxColor = 'box';
+
+
+// Ciclo da 1 a 100
+
+
+for(let i = 1; i <=100; i++){
+
+    if (i % 3 === 0 && i % 5 === 0){
+        valore = fizz + buzz;
+        boxColor = 'box-purple'
+
+    } else if (i % 3 === 0) {
+        output = 'Fizz';
+        boxColor = 'box-green';
+
+    } else if (i % 5 === 0) {
+        output = 'Buzz';
+        boxColor = 'box-ywl';
+        
+    } else {
+        output = i;
+        boxColor = 'box-blue'
+    }
+
+    const squareBox = document.createElement("div");
+    squareBox.className = `box ${boxColor}`;
+    squareBox.textContent = output;
+    
+    boxEl.append(squareBox);
+
+}
+
+
+
+
+
+
